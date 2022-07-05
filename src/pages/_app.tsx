3eab25eb225b22693e63cdad6cdc5224d5bc62ce
Diego Araújo <brhdn050654@ambev.com.br>
2022-07-05 15:56:14 -0300

    Custom Document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import {AppProps} from 'next/app'
 
+//Esse arquivo acessa todos os componentes que estão dentro de pages.
+//Funciona de maneira parecida a context.
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
---
+++ src/pages/_document.tsx
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+//Esse componente é renderizado apenas uma única vez
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500;600&family=Roboto:wght@400;700;900&display=swap" rel="stylesheet"/>
+        <title>ig.news</title>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
