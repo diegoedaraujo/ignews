35f9d6e40d8fc98051f47c2382127c8d9c69a246
Diego Araújo <brhdn050654@ambev.com.br>
2022-07-14 11:24:40 -0300

    Finalizando o componente Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
 import {AppProps} from 'next/app'
+import { Header } from '../components/Header'
 import '../styles/global.scss'
 
 //Esse arquivo acessa todos os componentes que estão dentro de pages.
 //Funciona de maneira parecida a context.
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Header />
+    <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
