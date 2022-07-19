5d18466fdbe6f9a2b9a97f1efcc6933958b3a3d3
Diego Araújo <brhdn050654@ambev.com.br>
2022-07-19 10:48:49 -0300

    Finalizando SignInButton

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,9 +1,29 @@
 import {FaGithub} from 'react-icons/fa'
+import styles from './styles.module.scss'
+import {FiX} from 'react-icons/fi'
 
 export function SignInButton() {
-    return (
-        <button type="button">
-            <FaGithub />
+    const isUserLoggedIn = true;
+    return isUserLoggedIn ? (
+        (
+            <button 
+            type="button"
+            className={styles.signInButton}
+            >
+                <FaGithub color='#04d361'/>
+                Diego Araújo
+                <FiX 
+                color='#737380'
+                className={styles.closeIcon}
+                />
+            </button>
+        )
+    ): (
+        <button 
+        type="button"
+        className={styles.signInButton}
+        >
+            <FaGithub color='#eba417'/>
             Sign in with Github
         </button>
     )
