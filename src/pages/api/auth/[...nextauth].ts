e56d1db2ee7ae1beb6130317cb4a3f0689d88584
Diego Araújo <brhdn050654@ambev.com.br>
2022-08-05 15:56:06 -0300

    Implementando insert no faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { fauna } from "../../../services/fauna"
+import { query as q } from "faunadb"
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -11,8 +13,19 @@ export default NextAuth({
         params: {
             scope: 'read:user',
         }
-      }    
+      },    
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      // const {}
+      // await fauna.query(
+      //   q.Create(
+      //     q.Collection('users'),
+      //   )
+      //   )
+      return true
+    },
+  }
 })
