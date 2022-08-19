0ea5f8185bf2cd535c104d072004b675d5f1e9e7
Diego Araújo <brhdn050654@ambev.com.br>
2022-08-19 14:28:24 -0300

    Implementando o subscribe

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
-import { fauna } from "../../../services/fauna"
-import { query as q } from "faunadb"
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { fauna } from "../../../services/fauna";
+import { query as q } from "faunadb";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -9,23 +9,33 @@ export default NextAuth({
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      authorization:{
+      authorization: {
         params: {
-            scope: 'read:user',
-        }
-      },    
+          scope: "read:user",
+        },
+      },
     }),
     // ...add more providers here
   ],
   callbacks: {
-    async signIn({ user, account, profile, email, credentials }) {
-      // const {}
-      // await fauna.query(
-      //   q.Create(
-      //     q.Collection('users'),
-      //   )
-      //   )
-      return true
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
+          )
+        );
+        return true;
+      } catch {
+        return false;
+      }
     },
-  }
-})
+  },
+});
