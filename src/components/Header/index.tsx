5fe649c64f990788afd2918d705b1056b5ca0634
Diego Araújo <brhdn050654@ambev.com.br>
2022-09-13 11:34:49 -0300

    Otimizando Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,22 @@
-import { SignInButton } from '../SignInButton';
-import styles from './styles.module.scss';
+import Link from "next/link";
+import { SignInButton } from "../SignInButton";
+import styles from "./styles.module.scss";
 
 export function Header() {
-
-    return (
-        <header className={styles.headerContainer}>
-            <div className={styles.headerContent}>
-                <img src="/images/logo.svg" alt="ig.news"/>
-                <nav>
-                    <a className={styles.active}>Home</a>
-                    <a>Posts</a>
-                </nav>
-                <SignInButton />
-            </div>
-        </header>
-    )
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/posts">
+            <a>Posts</a>
+          </Link>
+        </nav>
+        <SignInButton />
+      </div>
+    </header>
+  );
 }
