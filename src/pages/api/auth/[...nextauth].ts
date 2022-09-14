8b62941cf30d18db1e50a0a523f4902dbb8f843e
Diego Araújo <brhdn050654@ambev.com.br>
2022-09-14 16:46:47 -0300

    Tentando novamente

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -29,8 +29,7 @@ export default NextAuth({
                   "ref",
                   q.Get(
                     q.Match(
-                      q.Index("user_by_email"),
-                      q.Casefold(session.user.email)
+                      q.Index("user_by_email", q.Casefold(session.user.email))
                     )
                   )
                 )
